d31d50a2c3eb164fe0d31180b61fe76794a53d1b
Daria Tatarinova <dt.dashenka@mail.ru>
2023-01-06 20:41:12 +1000

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import sum from './basic';
+import Game, {GameSavingData, readGameSaving, writeGameSaving} from './game.js';
+
+const game = new Game();
+game.start();
+console.log('app worked');
+
+
